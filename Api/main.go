028fe8e783ede299e48cabc5f4fcc7fc315d7a09
Ken Liem <95730901+SleepyKen@users.noreply.github.com>
2024-09-09 10:03:53 +1000

    Added Functions and personalScore struct

--- Api/main.go
+++ Api/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type User struct {
@@ -11,6 +14,15 @@ type User struct {
 	University     string `json:"University"`
 	Username       string `json:"Username"`
 	ProfilePicture string `json:"ProfilePicture"`
+	Scores         *personalScore
+}
+type personalScore struct {
+	HighScoreGame1 int
+	HighScoreGame2 int
+	HighScoreGame3 int
+	HighScoreQuiz1 int
+	HighScoreQuiz2 int
+	HighScoreQuiz3 int
 }
 
 var Users []User
@@ -20,11 +32,32 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: homePage")
 }
 
+func getUsers(w http.ResponseWriter, r *http.Request) {
+
+	json.NewEncoder(w).Encode(Users)
+}
+func getUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+}
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var newUser User
+	_ = json.NewDecoder(r.Body).Decode(&newUser)
+	Users = append(Users, newUser)
+	json.NewEncoder(w).Encode(newUser)
+}
+
+func updateScore(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func handleRequests() {
+	router := mux.NewRouter()
 	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":8081", router))
 }
 
 func main() {
+	Users = append(Users, User{ID: 1, University: "Testing University", Username: "test", ProfilePicture: "Picture"})
 	handleRequests()
 }
