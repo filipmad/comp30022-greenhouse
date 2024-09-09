e8d9a460d6c80d0e48304b6246f84198fa96832c
Ken Liem <95730901+SleepyKen@users.noreply.github.com>
2024-09-09 10:46:51 +1000

    Added Functionality to getUsers and getUser

--- Api/main.go
+++ Api/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 type User struct {
-	ID             int    `json:"ID"`
+	ID             string `json:"ID"`
 	University     string `json:"University"`
 	Username       string `json:"Username"`
 	ProfilePicture string `json:"ProfilePicture"`
@@ -29,6 +29,7 @@ var Users []User
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
+
 	fmt.Println("Endpoint Hit: homePage")
 }
 
@@ -38,26 +39,42 @@ func getUsers(w http.ResponseWriter, r *http.Request) {
 }
 func getUser(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
+	for _, item := range Users {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+		json.NewEncoder(w).Encode(&User{})
 
+	}
 }
 func createUser(w http.ResponseWriter, r *http.Request) {
-	var newUser User
-	_ = json.NewDecoder(r.Body).Decode(&newUser)
-	Users = append(Users, newUser)
-	json.NewEncoder(w).Encode(newUser)
+	// var newUser User
+	// _ = json.NewDecoder(r.Body).Decode(&newUser)
+
+	// Users = append(Users, newUser)
+	// json.NewEncoder(w).Encode(newUser)
 }
 
 func updateScore(w http.ResponseWriter, r *http.Request) {
 
 }
+func testUsers(router *mux.Router) {
+
+	router.HandleFunc("/Users", createUser).Methods("POST")
+	router.HandleFunc("/Users", getUsers).Methods("GET")
+}
 
 func handleRequests() {
+	//Creates new mux router
 	router := mux.NewRouter()
-	http.HandleFunc("/", homePage)
+	router.HandleFunc("/", homePage)
+	testUsers(router)
 	log.Fatal(http.ListenAndServe(":8081", router))
+
 }
 
 func main() {
-	Users = append(Users, User{ID: 1, University: "Testing University", Username: "test", ProfilePicture: "Picture"})
+	Users = append(Users, User{ID: "1", University: "Testing University", Username: "test", ProfilePicture: "Picture"})
 	handleRequests()
 }
