1d9cdad33f8ea08f1c59da7ffe2c5ceb17321fc7
Leo Brooks <leo@brooksaustralia.com>
2024-09-10 23:41:08 +1000

    Added basic structure to title and navigation

--- App/greenhouse-app/src/App.js
+++ App/greenhouse-app/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function NavButton({text}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button class="navButton">{text}</button>
   );
 }
 
-export default App;
+function Title() {
+  return (
+    <div class="title">
+      <h1>Greenhouse</h1>
+    </div>
+  )
+}
+
+// To be replaced with our logo (upload logo to public)
+function Logo() {
+  return(
+    <div class="logo">
+      <img src={`${process.env.PUBLIC_URL}/logo192.png`} alt="Logo" />
+    </div>
+  )
+}
+
+function SettingsButton({type}) {
+  let img;
+  if(type == "search") {
+    img = "search.svg";
+  }
+  else {
+    img = "account.svg";
+  }
+  
+  return (
+    <div class="settingsButton">
+      <img src={`${process.env.PUBLIC_URL}/${img}`} alt="Logo" />
+    </div>
+  )
+}
+
+function SettingsSection() {
+  return (
+    <div class="settingsSection">
+      <SettingsButton type="search" />
+      <SettingsButton type="account" />
+    </div>
+  )
+}
+
+function MainNav() {
+  return(
+    <div>
+      <nav class="mainNav">
+        <NavButton text="Home" />
+        <NavButton text="Discover" />
+        <NavButton text="Games" />
+        <NavButton text="Grow" />
+        <NavButton text="News" />
+        <NavButton text="Community" />
+      </nav>
+    </div>
+  )
+}
+
+export default function myApp() {
+  return (
+    <div>
+      <Logo />
+      <Title />
+      <SettingsSection />
+
+      <MainNav />
+    </div>
+  );
+}
