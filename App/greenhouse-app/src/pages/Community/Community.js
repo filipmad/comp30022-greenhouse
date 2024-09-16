5265563e2b17f79613137c1562abed19eef64c0b
Leo Brooks <leo@brooksaustralia.com>
2024-09-17 07:29:10 +1000

    Added structure to community page

--- App/greenhouse-app/src/pages/Community/Community.js
+++ App/greenhouse-app/src/pages/Community/Community.js
@@ -1,7 +1,27 @@
+import React from "react";
+import "./Community.css";
+
 export default function Community() {
-  return (
-	<div>
-	  <h1>Community</h1>
-	</div>
-  );
-}
+	return (
+	  <div className="community-container">
+		<div className="section polls">
+		  <h2>Polls</h2>
+		  <div className="element">Poll 1</div>
+		  <div className="element">Poll 2</div>
+		  <div className="element">Poll 3</div>
+		</div>
+		<div className="section posts">
+		  <h2>Top Posts</h2>
+		  <div className="element">Post 1</div>
+		  <div className="element">Post 2</div>
+		  <div className="element">Post 3</div>
+		</div>
+		<div className="section milestones">
+		  <h2>Milestones</h2>
+		  <div className="element">Milestone 1</div>
+		  <div className="element">Milestone 2</div>
+		  <div className="element">Milestone 3</div>
+		</div>
+	  </div>
+	);
+  }
