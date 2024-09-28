4b5031e459dda63ca978fbd47184c051cf4d234c
Ken Liem <95730901+SleepyKen@users.noreply.github.com>
2024-09-28 12:04:41 +1000

    Added NewsPost and Poll struct and added struct tags to all structs in forum.go

--- Api/forum.go
+++ Api/forum.go
@@ -3,19 +3,38 @@ package main
 import "time"
 
 type ForumPost struct {
-	authorID   int
-	postID     int
-	title      string
-	text       string
-	likes      int
-	datePosted time.Time
+	AuthorID   int       `json:"AuthorID"`
+	PostID     int       `json:"PostID"`
+	Title      string    `json:"Title"`
+	Text       string    `json:"Text"`
+	Likes      int       `json:"Text"`
+	DatePosted time.Time `json:"DatePosted"`
 }
 
 type Comment struct {
-	commentID       int
-	text            string
-	datePosted      time.Time
-	postID          int
-	postAuthorID    int
-	commentAuthorID int
+	CommentID       int       `json:"CommentID"`
+	Text            string    `json:"Text"`
+	DatePosted      time.Time `json:"DatePosted"`
+	PostID          int       `json:"PostID"`
+	PostAuthorID    int       `json:"PostAuthorID"`
+	CommentAuthorID int       `json:"CommentAuthorID"`
+}
+
+type NewsPost struct {
+	NewsPostID   int       `json:"CommentID"`
+	Title        string    `json:"Title"`
+	Author       string    `json:"Author"`
+	Text         string    `json:"Text"`
+	IsNewsLetter bool      `json:"IsNewsLetter"`
+	Timecreated  time.Time `json:"TimeCreated"`
+}
+
+type Poll struct {
+	PollID         int    `json:"PollID"`
+	Title          string `json:"Title"`
+	Text           string `json:"Text"`
+	OptionOne      string `json:"OptionOne"`
+	OptionTwo      string `json:"OptionTwo"`
+	OptionOneVotes int    `json:"OptionOneVotes"`
+	OptionTwoVotes int    `json:"OptionTwoVotes"`
 }
