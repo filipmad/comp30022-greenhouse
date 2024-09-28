a5b0e82a16315cd65e965ef4fa114630c1e3595c
WestBeast08 <leo@brooksaustralia.com>
2024-09-28 18:05:08 +1000

    Added iframe to support phaser game's index.html for WordExplorer

--- App/greenhouse-app/src/pages/Games/WordExplorer/WordExplorer.js
+++ App/greenhouse-app/src/pages/Games/WordExplorer/WordExplorer.js
@@ -1,7 +1,26 @@
+import React from 'react';
+
+const GameComponent = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Play the Game</h1>
+      <iframe
+        title="phaser-game"
+        src={`${process.env.PUBLIC_URL}/WordExplorerGame/index.html`}
+        width="800"
+        height="600"
+        frameBorder="0"
+        allowFullScreen
+      />
+    </div>
+  );
+};
+
 export default function WordExplorer() {
   return (
 	<div>
 	  <h1>WordExplorer</h1>
+	  <GameComponent />
 	</div>
   );
 }
